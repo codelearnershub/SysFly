03eb0ac75691205956771b6561de57593218cb52
YahyaOlalekan <mybluvedcreator@gmail.com>
2023-03-23 17:22:19 +0100

    company class

---
+++ AirlineMS/Models/Entities/Company.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirlineMS.Models.Entities
+{
+    public class Company : BaseEntity
+    {
+        public string UserId { get; set; }
+        public string CompanyName { get; set; }
+        public string CACRegistrationNum { get; set; }
+        public string CACDocument { get; set; }
+        public string Logo { get; set; }
+        public string Email { get; set; }
+        public string HQAddress { get; set; }
+        public string HQPhoneNumber { get; set; }
+        public List<Branch> Branches { get; set; }
+    }
+}

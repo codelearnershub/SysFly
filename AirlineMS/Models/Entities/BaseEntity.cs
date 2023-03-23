ecd5dafb241206f1462b2187ec7e5e2c723bf563
Oluwaseun011 <oluwaseunidowu011@gmail.com>
2023-03-23 16:36:16 +0100

    initial

---
+++ AirlineMS/Models/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace AirlineMS.Models.Entities
+{
+    public class BaseEntity
+    {
+        public string Id{get; set;} = Guid.NewGuid().ToString();
+        public bool IsDeleted{get;set;}
+        public DateTime DateCreated{get; set;}
+        public DateTime CreatedBy{get; set;}
+    }
+}
